38c1a12571571ebf220a4efa7369965b947be6aa
Hu_xiang <96944540+hujiawei-things@users.noreply.github.com>
2023-10-02 20:20:39 +0800

    modify speed

--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ class Settings:
         self.screen_width = 1500
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
+
+        self.ship_speed = 0.5
--- ship.py
+++ ship.py
@@ -7,20 +7,24 @@ class Ship:
     def __init__(self, ai_game):
         """初始化位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 转化为浮点数
+        self.x = float(self.rect.x)
         # 移动标志
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
