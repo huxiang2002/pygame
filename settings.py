865d90edcffb43e9b339bc251d50e2663d946585
Hu_xiang <96944540+hujiawei-things@users.noreply.github.com>
2023-10-03 11:01:26 +0800

    add bullets

--- settings.py
+++ settings.py
@@ -9,3 +9,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         self.ship_speed = 1
+
+        # 子弹设置
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
